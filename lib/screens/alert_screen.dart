0ced7d68e5c0606495c4a1b119d6ded062546165
jrocoh <jrocoh@gmail.com>
2024-03-31 03:14:15 -0300

    add AlertDialog and update theme

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,16 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialogIos(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Titulo'),
+          content: const Column(
+            children: [
+              Text('contenido del alert'),
+              FlutterLogo(),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          title: const Text('Titulo'),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('este es el contenido del alert'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Alert'),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Mostrar alert'),
+          onPressed: () => Platform.isAndroid
+              ? displayDialogAndroid(context)
+              : displayDialogIos(context),
+        ),
       ),
-      body: const Center(
-        child: Text('AlertScreen'),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,14 +4,29 @@ class AppTheme {
   static const Color primary = Colors.lightGreen;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
+    //primary color
     primaryColor: primary,
+    //AppBar
     appBarTheme: const AppBarTheme(
       backgroundColor: primary,
       elevation: 0.0,
     ),
+    //Icon
     primaryIconTheme: const IconThemeData(color: primary),
+    //TextButton
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(foregroundColor: primary),
+    ),
+    //Floating Action Button
+    floatingActionButtonTheme:
+        const FloatingActionButtonThemeData(backgroundColor: primary),
+    //ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: primary,
+        shape: const StadiumBorder(),
+        elevation: 0,
+      ),
     ),
   );
 }
